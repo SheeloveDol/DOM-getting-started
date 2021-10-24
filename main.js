9d5ee0a2ddb40ce8cfef9fdad2727068fac76d51
Sheelove Dol <siesel26@gmail.com>
2021-10-23 19:41:33 -0700

    Create an element and add it to li

--- main.js
+++ main.js
@@ -2,9 +2,15 @@ document.querySelector('h1').innerHTML = 'Most popular TV show searchees in 2016
 
 document.getElementById('fourth').innerHTML = 'Fourth element';
 document.getElementById('second').innerHTML = 'What is going on?';
+
 let nextInLine = document.getElementById('third');
+nextInLine.style.fontSize = '2em';
 
 let body = document.body;
 body.style.backgroundColor = '#407F3E';
 
-nextInLine.style.fontSize = '2em';
+// creating an element:
+let newDestination = document.createElement('li');
+newDestination.id = 'oaxaca';
+newDestination.innerHTML = 'Oaxaca, Mexico';
+document.querySelector('ol').appendChild(newDestination);
