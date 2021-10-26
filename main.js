029d4e1d5b5077eb58b56a1c0a4cb5966e4ab7d2
Sheelove Dol <siesel26@gmail.com>
2021-10-25 22:25:54 -0700

    Add example of addEventListeners()

--- main.js
+++ main.js
@@ -55,3 +55,36 @@ function turnBackgroundBlue() {
 element.onclick = turnBackgroundBlue;
 
 
+// Example of even listeners being created and applied
+
+
+/*
+let view = document.getElementById('view-button');
+let close = document.getElementById('close-button');
+let codey = document.getElementById('codey');
+
+let open = function() {
+  codey.style.display = 'block';
+  close.style.display = 'block';
+};
+
+let hide = function() {
+  codey.style.display = 'none';
+  close.style.display = 'none';
+};
+
+view.addEventListener('click', open);
+close.addEventListener('click', hide);
+
+// Write your code here
+let textChange = function() {
+  view.innerHTML = 'Hello, World!';
+}
+
+let textReturn = function() {
+  view.innerHTML = 'View';
+}
+
+view.addEventListener('click', textChange);
+close.addEventListener('click', textReturn);
+*/
